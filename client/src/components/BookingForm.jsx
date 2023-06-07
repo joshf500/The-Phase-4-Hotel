76dede22b860dedb2e85a45cc4e8d959f22f25ad
Natasha Bartley <+>
2023-06-07 03:49:17 -0400

    newBooking form setup

---
+++ client/src/components/BookingForm.jsx
@@ -0,0 +1,77 @@
+import 'react-modern-calendar-datepicker/lib/DatePicker.css';
+import DatePicker from 'react-modern-calendar-datepicker';
+import { useState } from 'react';
+
+const BookingForm = () => {
+  const [guests, setGuests] = useState(1);
+  const [selectedDays, setSelectedDays] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleGuestsChange = (e) => {
+    setGuests(parseInt(e.target.value));
+  };
+
+  const handleDateChange = (selectedDay) => {
+    setSelectedDays(selectedDay);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+
+    const formData = {
+      guests: guests,
+      startDate: selectedDays ? selectedDays.from : null,
+      endDate: selectedDays ? selectedDays.to : null,
+    };
+
+    fetch('/api/bookings', { //Update!!!
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(formData),
+    })
+      .then((response) => {
+        setIsLoading(false);
+        if (response.ok) {
+          // Handle successful response
+          console.log('Booking submitted successfully!');
+        } else {
+          // Handle error response
+          console.log('Failed to submit booking.');
+        }
+      })
+      .catch((error) => {
+        setIsLoading(false);
+        console.error('An error occurred:', error);
+      });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="guests">Number of Guests:</label>
+      <input
+        type="number"
+        id="guests"
+        value={guests}
+        onChange={handleGuestsChange}
+        min="1"
+      />
+
+      <label htmlFor="date">Date Range:</label>
+      <DatePicker
+        value={selectedDays}
+        onChange={handleDateChange}
+        inputPlaceholder="Select a date range"
+        shouldHighlightWeekends
+      />
+
+      <button type="submit" disabled={isLoading}>
+        {isLoading ? 'Submitting...' : 'Book Now'}
+      </button>
+    </form>
+  );
+};
+
+export default BookingForm;
--- client/src/pages/BookingPage.jsx
+++ client/src/pages/BookingPage.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import ParallaxHeader from '../components/ParallaxHeader';
+import BookingForm from '../components/BookingForm';
 
 const BookingPage = () => {
   return (
     <div>
-  
+      <BookingForm />
     </div>
   );
 };
