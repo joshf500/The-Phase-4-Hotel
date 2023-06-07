3a5ac3c2801a7d7f31f3777ee0ef388ab166b449
Natasha Bartley <+>
2023-06-07 16:21:27 -0400

    set const for SignUp

--- client/src/components/SignUp.jsx
+++ client/src/components/SignUp.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 
-function SignUp(){
+function SignUp({ onSignUp }){
+    const [handleSubmit, sethandleSubmit] = useState('login');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const [errors, setErrors] = useState([]);
+    
     return(
         <div>
 
