3ec3810336fb356a091e77a5d87cb818a5d4b4c1
Natasha Bartley <+>
2023-06-08 12:59:00 -0400

    RoomCard return

--- client/src/components/RoomCard.jsx
+++ client/src/components/RoomCard.jsx
@@ -14,3 +14,27 @@ function RoomCard() {
         console.error("Error fetching room data:", error);
       });
   }, []);
+  
+  return (
+    <div className="room-cards">
+      {rooms.map((room) => (
+        <div className="room-card" key={room.id}>
+          <img className="room-image" src={room.image_url} alt="Room" />
+          <div className="room-details">
+            <h2 className="room-number">{room.room_number}</h2>
+            <p className="room-price">Price per night: ${room.price_per_night}</p>
+            <p className="room-sleeps">Sleeps: {room.sleeps}</p>
+            <p className="room-beds">Queen beds: {room.queen_beds}</p>
+            <p className="room-pullout-beds">Couch pullout beds: {room.couch_pullout_beds}</p>
+            <p className="room-view">Special view: {room.special_view}</p>
+            <button className="choose-room-button" onClick={() => handleReserveClick(room)}>
+              Reserve this room
+            </button>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default RoomCard;
