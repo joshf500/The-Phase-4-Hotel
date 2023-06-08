8d4317fa8254d827d77e5e0ee68a4922acefed41
Natasha Bartley <+>
2023-06-08 13:09:45 -0400

    revised RoomList

--- client/src/components/RoomsList.jsx
+++ client/src/components/RoomsList.jsx
@@ -1,18 +1,18 @@
 import React from "react";
-import RoomCard from './RoomCard.jsx';
+import RoomCard from "./RoomCard.jsx";
 
 function RoomList({ rooms }) {
   if (!rooms) {
     return null; // Handle the case when rooms are not available
   }
 
-  const ArrayOfRooms = rooms.map((room) => {
-    return <RoomCard room={room} key={room.name} />;
+  const arrayOfRooms = rooms.map((room) => {
+    return <RoomCard room={room} key={room.id} />;
   });
 
   return (
     <div id="RoomListContainer">
-      <ul>{ArrayOfRooms}</ul>
+      <ul>{arrayOfRooms}</ul>
     </div>
   );
 }
