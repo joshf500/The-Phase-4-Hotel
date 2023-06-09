4e604db159061723e6d34fb8b9e0a6cf6bd7d9ae
Natasha Bartley <+>
2023-06-08 21:07:12 -0400

    user auth is good enough

--- client/src/components/UserAuth.jsx
+++ client/src/components/UserAuth.jsx
@@ -152,13 +152,9 @@ function UserAuth({ onSignUp, onLogin }) {
                 </div>
               </fieldset>
               <button type="submit" className="btn-signup" onClick={handleSignUpClick}>Sign Up</button>
+              {isRegistered && <div>You are now registered!</div>}
             </form>
 
-              
-        <div> 
-          <section className="auth-forms-section">
-            <h1 className="auth-section-title">your getaway awaits</h1>
-            <div className="auth-forms">
               <div className={`auth-form-wrapper ${activeForm === 'login' ? 'is-active' : ''}`}>
                 <button
                   type="button"
@@ -194,18 +190,138 @@ function UserAuth({ onSignUp, onLogin }) {
                             </div>
                           </fieldset>
                           <button type="submit" className="btn-login" onClick={handleLoginClick}>Login</button>
+                          {isLoggedIn && <div>You are now logged in!</div>}
                         </form>
                       </div>
                     </div>
-                    {isLoggedIn && <div>You are now logged in!</div>}
-                  </section>
                 </div>
-
-            </div>
-          </div>
-          {isRegistered && <div>You are now registered!</div>}
         </section>
       );
     }
 
   export default UserAuth;
+
+
+
+
+
+
+
+
+
+
+
+  
+/*
+
+return (
+  <section className="auth-forms-section">
+    <h1 className="auth-section-title">your getaway awaits</h1>
+    <div className="auth-forms">
+      <div className={`auth-form-wrapper ${activeForm === 'login' ? 'is-active' : ''}`}>
+        <button
+          type="button"
+          className="switcher auth-switcher-login"
+          onClick={() => handleSwitchForm('login')}
+        >
+          Login
+          <span className="auth-title-underline"></span>
+        </button>
+        <form className="AuthForm form-login" onSubmit={HandleAuthFormSubmit}>
+          <fieldset>
+            <legend>Please, enter your email and password for login.</legend>
+            <div className="input-block">
+              <label htmlFor="login-email">E-mail</label>
+              <input 
+                id="login-email" // edit
+                type="email" required 
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+            <div className="input-block">
+              <label htmlFor="login-password">Password</label>
+              <input 
+                placeholder="login-password" 
+                type="password" required 
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+          </fieldset>
+          <button type="submit" className="btn-login" disabled={isLoading}>
+            {isLoading ? 'Logging in...' : 'Login'}
+          </button>
+          {errors.length > 0 && (
+            <div className="error-messages">
+              <ul>
+                {errors.map((error, index) => (
+                  <li key={index}>{error}</li>
+                ))}
+              </ul>
+            </div>
+          )}
+        </form>
+      </div>
+      <div className={`auth-form-wrapper ${activeForm === 'signup' ? 'is-active' : ''}`}>
+        <button
+          type="button"
+          className="switcher auth-switcher-signup"
+          onClick={() => handleSwitchForm('signup')}
+        >
+          Sign Up
+          <span className="auth-title-underline"></span>
+        </button>
+        <form className="AuthForm form-signup" onSubmit={HandleAuthFormSubmit}>
+          <fieldset>
+            <legend>Please, enter your email, password, and password confirmation for sign up.</legend>
+            <div className="input-block">
+              <label htmlFor="signup-email">E-mail</label>
+              <input 
+                placeholder="signup-email"
+                type="email" required
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                />
+            </div>
+            <div className="input-block">
+              <label htmlFor="signup-password">Password</label>
+              <input 
+                placeholder="signup-password" 
+                type="password" required 
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+            <div className="input-block">
+              <label htmlFor="signup-password-confirm">Confirm password</label>
+              <input 
+                id="signup-password-confirm" 
+                type="password" required 
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+              />
+            </div>
+          </fieldset>
+          <button type="submit" className="-auth-btn-signup" disabled={isLoading}>
+            {isLoading ? 'Signing up...' : 'Continue'}
+          </button>
+          {errors.length > 0 && (
+            <div className="error-messages">
+              <ul>
+                {errors.map((error, index) => (
+                  <li key={index}>{error}</li>
+                ))}
+              </ul>
+            </div>
+          )}
+        </form>
+      </div>
+    </div>
+  </section>
+);
+}
+
+export default UserAuth;
+
+*/
